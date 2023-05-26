aeb58a1275d50cf3a2bf24aae7c972926140c153
babunewgit1 <babunewx11@gmail.com>
2023-05-26 14:59:24 +0600

    update firebase file

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAuKxNVPBwbpJIvsjZshCoqPdgZis5yuqk",
-  authDomain: "chef-cdaec.firebaseapp.com",
-  projectId: "chef-cdaec",
-  storageBucket: "chef-cdaec.appspot.com",
-  messagingSenderId: "1063493673090",
-  appId: "1:1063493673090:web:0be0c06c2971415074149b",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
